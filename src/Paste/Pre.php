abd6725700c5bcdb83526f3e5fc6cd85756e911e
Aaron Oxborrow <aaron@pastedesign.com>
2013-05-24 16:39:38 -0700

    move tests/examples to Pre repo, move json_pretty

--- src/Paste/Pre.php
+++ src/Paste/Pre.php
@@ -39,6 +39,57 @@ class Pre {
 		return self::render($data, $label);
 	}
 
+
+	// pretty print JSON string
+	// http://www.daveperrett.com/articles/2008/03/11/format-json-with-php/
+	public static function json($json) {
+		
+		$result      = '';
+		$pos         = 0;
+		$strLen      = strlen($json);
+		$indentStr   = '  ';
+		$newLine     = "\n";
+		$prevChar    = '';
+		$outOfQuotes = TRUE;
+
+		for ($i = 0; $i <= $strLen; $i++) {
+
+			// Grab the next character in the string.
+			$char = substr($json, $i, 1);
+
+			// Are we inside a quoted string?
+			if ($char == '"' && $prevChar != '\\') {
+				$outOfQuotes = !$outOfQuotes;
+
+			// If this character is the end of an element,
+			// output a new line and indent the next line.
+			} else if (($char == '}' || $char == ']') && $outOfQuotes) {
+				$result .= $newLine;
+				$pos --;
+				for ($j=0; $j<$pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+			// Add the character to the result string.
+			$result .= $char;
+
+			// If the last character was the beginning of an element,
+			// output a new line and indent the next line.
+			if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+				$result .= $newLine;
+				if ($char == '{' || $char == '[')
+					$pos ++;
+				for ($j = 0; $j < $pos; $j++)
+					$result .= $indentStr;
+			}
+
+			$prevChar = $char;
+		}
+
+		return $result;
+
+	}
+
 	
 	// simple wrapper for var_dump that outputs within a styled <pre> tag and fixes some whitespace and formatting
 	public static function render($data, $label = NULL) {
--- test/index.php
+++ test/index.php
@@ -10,6 +10,47 @@ require '../src/Paste/Pre.php';
 // namespace shortcut
 use Paste\Pre as Pre;
 
-// testing Pre lib
-echo Pre::render($_SERVER);
+
+// test class
+class PreObject {
+	
+	public $public = 'public';
+	protected $protected = 'protected';
+	private $private = 'private';
+	public static $static = 'static';
+	
+	public function __construct() {
+		$this->runtime = 'runtime';
+	}
+	
+	public function func() {
+		return 'are we having func() yet?';
+	}
+}
+
+// Pretty JSON
+$json = file_get_contents('sample.json');
+$pretty_json = Pre::json($json);
+echo Pre::render($pretty_json, 'json:');
+
+// PreObject
+$po = new PreObject;
+$po->member = new StdClass;
+$po->member->property = 'property';
+$po->member->submember = new PreObject;
+echo Pre::render($po, 'PreObject Test:');
+
+// StdClass
+$sc = new StdClass;
+$sc->property = 'property';
+$sc->bool = TRUE;
+$sc->float = 5.678;
+$sc->int = 678;
+$sc->null = NULL;
+$sc->member = $po;
+$sc->member->submember = new PreObject;
+echo Pre::render($sc, 'StdClass Test:');
+
+echo Pre::render($_SERVER, '_SERVER:');
+
 
